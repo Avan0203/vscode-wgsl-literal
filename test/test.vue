ea03ca5dd6c0512f9dd4698a9ad0d8cd1f9a1124
wuyifan <wuyifan@udschina.com>
2026-07-30 11:05:28 +0800

    Add WGSL syntax support for Markdown and enhance existing syntax definitions

---
+++ test/test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+const shader = /* wgsl */ `
+    @vertex fn vs(
+        @builtin(vertex_index) vertexIndex : u32
+    ) -> @builtin(position) vec4f {
+        return vec4f(0.0, 0.0, 0.0, 1.0);
+    }
+`;
+</script>
+
+
